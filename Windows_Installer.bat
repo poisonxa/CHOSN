b6f4db24fc117f4be78af06db20cb7d1b39fff82
Nesci28 <33325461+Nesci28@users.noreply.github.com>
2018-02-01 20:56:21 -0500

    Update Windows_Installer.bat

--- Windows_Installer.bat
+++ Windows_Installer.bat
@@ -18,18 +18,17 @@ echo.
 echo.
 echo.
 echo.
-<nul set /p "=Enter to continue..."
+<nul set /p "=Press Enter to continue..."
 pause >nul
 cls
+
+::#######################################
 
 ::############ WIFI SETTINGS ############
 
 :whileWIFI
 	@echo [WIFI Settings]>> 1Bash.ini
 	set /p WIFI=Is your rig connected by WIFI [y/n]?
-	if %WIFI%==clear (
-		GOTO :eraseWIFI
-	)
 	if %WIFI%==y (
 		set result=true
 	)
@@ -43,18 +42,39 @@ cls
 	@echo WIFI=%WIFI%>> 1Bash.ini
 	if %WIFI%==y (
 		set /p WIFISSID=What is you Network SSID?
-		if !WIFISSID!==clear (
-			GOTO :eraseWIFI
-		)
 		@echo WIFI_SSID=!WIFISSID!>> 1Bash.ini
 		set /p WIFIPASS=What is you Network PASSWORD?
-		if !WIFIPASS!==clear (
-			GOTO :eraseWIFI
-		)
 		@echo WIFI_PASS=!WIFIPASS!>> 1Bash.ini
+	)
+	:WIFIConfirm
+	cls
+	echo ###############################################
+	echo ############## WIFI Confirmation ##############
+	echo ###############################################
+	echo.
+	echo.
+	echo.
+	echo Using Wifi = %WIFI%
+	echo SSID = %WIFISSID%
+	echo Password = %WIFIPASS%
+	echo.
+	set /p WIFIConfirm=Is all the informations OK [y/n]?
+	if %WIFIConfirm%==n (
+		set result1=true
+		cls
+		GOTO :eraseWIFI
+	)
+	if %WIFIConfirm%==y (
+		set result1=true
+	)
+	if NOT "%result1%"=="true" (
+		echo Error in your answer, must be [y/n]
+		cls
+		GOTO :WIFIConfirm
 	)
 	set result=false
 	set result1=false
+	set result2=false
 	cls
 	
 ::#######################################
@@ -66,54 +86,33 @@ cls
 	@echo [WTM Settings]>> 1Bash.ini
 	:whileWTM2
 	set /p WTM=Are you going to use WTM Auto-Switcher [y/n]?
-	if %WTM%==clear (
-		GOTO :eraseWTM
-	)
 	if %WTM%==y (
 		set result=true
-		@echo WTM=%WTM%>> 1Bash.ini
+		@echo WTM=!WTM!>> 1Bash.ini
 		:whileWTM3
 		set /p WTM_PROFIT=Do you want to use the profit checker [y/n]?
-		if !WTM_PROFIT!==clear (
-			GOTO :eraseWTM
-		)
-		if !WTM_PROFIT!=y (
+		if !WTM_PROFIT!==y (
 			set result1=true
-			@echo WTM_PROFIT=%WTM_PROFIT%>> 1Bash.ini
+			@echo WTM_PROFIT=!WTM_PROFIT!>> 1Bash.ini
+			set /p WTM_PROFIT_TIME=Interval between profit check [in minutes;2]?
+			@echo WTM_PROFIT_TIME=!WTM_PROFIT_TIME!>> 1Bash.ini
 		)
-		if !WTM_PROFIT!=n (
+		if !WTM_PROFIT!==n (
 			set result1=true
-			@echo WTM_PROFIT=%WTM_PROFIT%>> 1Bash.ini
+			@echo WTM_PROFIT=!WTM_PROFIT!>> 1Bash.ini
 		)
-		if NOT "%result1%"=="true" (
+		if NOT "!result1!"=="true" (
 			echo Error in your answer, must be [y/n]
 			GOTO :whileWTM3
 		)
 		set /p WTM_COIN=What are the coins you want to use for WTM Auto-Switcher [Ex: ZEN;ZEC;MONA]?
-		if !WTM_COIN!==clear (
-			GOTO :eraseWTM
-		)
-		@echo WTM_COIN=%WTM_COIN%>> 1Bash.ini
+		@echo WTM_COIN=!WTM_COIN!>> 1Bash.ini
 		set /p WTM_SWITCH_TIME=Interval between switch check [in minutes;2]?
-		if !WTM_SWITCH_TIME!==clear (
-			GOTO :eraseWTM
-		)
-		@echo WTM_SWITCH_TIME=%WTM_SWITCH_TIME%>> 1Bash.ini
-		set /p WTM_PROFIT_TIME=Interval between profit check [in minutes;2]?
-		if !WTM_PROFIT_TIME!==clear (
-			GOTO :eraseWTM
-		)
-		@echo WTM_PROFIT_TIME=%WTM_PROFIT_TIME%>> 1Bash.ini
+		@echo WTM_SWITCH_TIME=!WTM_SWITCH_TIME!>> 1Bash.ini
 		set /p WTM_SWITCH_PERCENT=Minimum difference to switch to a new coin [in percentage;20]?
-		if !WTM_SWITCH_PERCENT!==clear (
-			GOTO :eraseWTM
-		)
-		@echo WTM_SWITCH_PERCENT=%WTM_SWITCH_PERCENT%>> 1Bash.ini
+		@echo WTM_SWITCH_PERCENT=!WTM_SWITCH_PERCENT!>> 1Bash.ini
 		set /p WTM_CURRENCY=Currency in which you want to use WTM?
-		if !WTM_CURRENCY!==clear (
-			GOTO :eraseWTM
-		)
-		@echo WTM_CURRENCY=%WTM_CURRENCY%>> 1Bash.ini
+		@echo WTM_CURRENCY=!WTM_CURRENCY!>> 1Bash.ini
 	)
 	if %WTM%==n (
 		set result=true
@@ -123,34 +122,40 @@ cls
 		echo Error in your answer, must be [y/n]
 		GOTO :whileWTM2
 	)
-	set result=false
-	set result1=false
+	:WTMConfirm
 	cls
-
-::#######################################
-	
-::############# 1OC SETTINGS ############	
-
-:whileOC
-	@echo.>> 1Bash.ini
-	@echo [1OC Settings]>> 1Bash.ini
-	:whileOC2
-	set /p OC=How to you want your overclocking [manual/auto]?
-	if %WTM%==clear (
+	echo ###############################################
+	echo ############### WTM Confirmation ##############
+	echo ###############################################
+	echo.
+	echo.
+	echo.
+	echo Using WTM = %WTM%
+	echo WTM Profit = %WTM_Profit%
+	echo WTM Profit Time = %WTM_PROFIT_TIME%
+	echo WTM Coin = %WTM_COIN%
+	echo WTM Switch Time = %WTM_SWITCH_TIME%
+	echo WTM Switch Percent = %WTM_SWITCH_PERCENT%
+	echo WTM Currency = %WTM_CURRENCY%
+	echo.
+	set /p WTMConfirm=Is all the informations OK [y/n]?
+	if %WTMConfirm%==n (
+		set result2=true
+		cls
 		GOTO :eraseWTM
 	)
-	if %OC%==manual (
-		set result=true
-		@echo OC=!OC!>> 1Bash.ini
+	if %WTMConfirm%==y (
+		set result2=true
 	)
-	if %OC%==auto (
-		set result=true
-		@echo OC=!OC!>> 1Bash.ini
+	if NOT "%result2%"=="true" (
+		echo Error in your answer, must be [y/n]
+		cls
+		GOTO :WTMConfirm
 	)
-	if NOT "%result%"=="true" (
-		echo Error in your answer, must be [manual/auto]
-		GOTO :whileOC2
-	)
+	set result=false
+	set result1=false
+	set result2=false
+	cls
 
 ::#######################################
 	
@@ -160,21 +165,12 @@ cls
 	@echo.>> 1Bash.ini
 	@echo [8COINS Settings]>> 1Bash.ini
 	set /p COIN=What is the coin you want to mine?
-	if %COIN%==clear (
-		GOTO :eraseCOIN
-	)
 	:whileCOIN2
 	@echo COIN=%COIN%>> 1Bash.ini
 	set /p WORKERNAME=Are you going to use a default Worker Name [y/n]?
-	if %WORKERNAME%==clear (
-		GOTO :eraseCOIN
-	)
 	if %WORKERNAME%==y (
 		set result=true
 		set /p COIN_WORKERNAME=What is going to be the default Worker Name?
-		if !COIN_WORKERNAME!==clear (
-			GOTO :eraseCOIN
-		)
 		@echo COIN_WORKER=%WORKERNAME%>> 1Bash.ini
 		@echo COIN_WORKERNAME=%COIN_WORKERNAME%>> 1Bash.ini
 	)
@@ -186,10 +182,61 @@ cls
 		echo Error in your answer, must be [y/n]
 		GOTO :whileCOIN2
 	)
+	:WTMConfirm
+	cls
+	echo ###############################################
+	echo ############## 8Coins Confirmation ############
+	echo ###############################################
+	echo.
+	echo.
+	echo.
+	echo Coin = %COIN%
+	echo Coin Worker = %WORKERNAME%
+	echo Coin Worker Name = %COIN_WORKERNAME%
+	echo.
+	set /p COINConfirm=Is all the informations OK [y/n]?
+	if %COINConfirm%==n (
+		set result2=true
+		cls
+		GOTO :eraseCOIN
+	)
+	if %COINConfirm%==y (
+		set result2=true
+	)
+	if NOT "%result2%"=="true" (
+		echo Error in your answer, must be [y/n]
+		cls
+		GOTO :COINConfirm
+	)
 	set result=false
 	set result1=false
+	set result2=false
 	cls
 	
+::#######################################
+
+::############# 1OC SETTINGS ############	
+
+:whileOC
+	@echo.>> 1Bash.ini
+	@echo [1OC Settings]>> 1Bash.ini
+	:whileOC2
+	set /p OC=How to you want your overclocking [manual/auto]?
+	if %OC%==manual (
+		set result=true
+		@echo OC=!OC!>> 1Bash.ini
+	)
+	if %OC%==auto (
+		set result=true
+		@echo OC=!OC!>> 1Bash.ini
+		set /p AUTO_GPU=How many GPUs in your RIG?
+		
+	)
+	if NOT "%result%"=="true" (
+		echo Error in your answer, must be [manual/auto]
+		GOTO :whileOC2
+	)
+
 ::#######################################
 
 ::########### ERASE FUNCTIONS ###########
